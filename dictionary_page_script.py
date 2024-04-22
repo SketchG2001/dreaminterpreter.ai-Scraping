8a5a6c77b22d79a3023514d0610d3bc61cfc4995
vikas <vikasgole089@gmail.com>
2024-04-23 00:02:04 +0530

    to this(https://dreaminterpreter.ai/dream-dictionary/a) type of page has been scraped all the defintion page is remaining

--- dictionary_page_script.py
+++ dictionary_page_script.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 import requests
 from selenium import webdriver
@@ -41,21 +42,76 @@ while not exit_loop:
     # Print the text content of each li tag
     # li tags to be ignored
     li_elements_to_process = li_tags[1:]
+    count = 0
     for li_tag in li_elements_to_process:
         # print(li_tag.text.strip())
+        dict_key = li_tag.text.strip()
+        # sys.exit()
         anchor_tag = li_tag.find('a')
         if anchor_tag:
             link = anchor_tag.get('href')
+            # print("link is: ",link)
             if link:
                 complete_link = 'https://dreaminterpreter.ai' + link
                 # print(complete_link)
-                driver.get(complete_link)
-                wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body")))
-                wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[3]/div/div')))
-                meaning_page_html = driver.page_source
-                meaning_page_soup = BeautifulSoup(meaning_page_html, 'html.parser')
-                print(meaning_page_soup.prettify())
-                sys.exit()
+                max_attempts = 5  # Maximum number of retry attempts
+                attempts = 0
+                while attempts < max_attempts:
+                    try:
+
+                        # with open("Data_Extracted/dictionary_data/dictionary_key_urls.txt", "a",encoding='utf-8') as key_urls:
+                        #     key_urls.write(f"{dict_key}: {complete_link}\n")
+
+                        driver.get(complete_link)
+                        wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body")))
+                        wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[3]/div/div')))
+                        meaning_page_html = driver.page_source
+                        meaning_page_soup = BeautifulSoup(meaning_page_html, 'html.parser')
+                        # print(meaning_page_soup.prettify())
+                        # with open(f"HTML _Extracted/dictionary_HTML/{dict_key}_key.html", 'a', encoding='utf-8') as file:
+                        #     file.write(meaning_page_soup.prettify())
+                        count += 1
+                        dict_def_div = meaning_page_soup.find('div',class_='Dictionary_definition_container__zX89x')
+                        # print(dict_def_div)
+                        with open("output_file.txt", "a", encoding='utf-8') as output_file:
+                            with open("hrefs_file.txt", "a", encoding='utf-8') as hrefs_file:
+                                if dict_def_div:
+                                    heading_tag = dict_def_div.find('h1')
+                                    if heading_tag:
+                                        heading_text = heading_tag.text.strip()
+                                        output_file.write(heading_text + "\n")
+                                    second_div = dict_def_div.find('div')
+                                    if second_div:
+                                        all_content_div = second_div.find_all('div')
+                                        if all_content_div:
+                                            for content_div in all_content_div:
+                                                subheading_anchor = content_div.find('a')
+                                                if subheading_anchor:
+                                                    # on the dictionary page as per passed key
+                                                    subheading = subheading_anchor.find('h3').text.strip()
+                                                    # https: // dreaminterpreter.ai / dream - dictionary / a
+                                                    subheading_href = subheading_anchor.get('href')
+                                                    output_file.write(subheading + "\n")
+                                                    hrefs_file.write(f'{count}: https://dreaminterpreter.ai{subheading_href}' + "\n")
+                                                paragraph = content_div.find('p').text.strip()
+                                                output_file.write(paragraph + "\n")
+
+                        # if count == 1:
+                        #     break
+
+
+
+                        break
+                    except TimeoutException:
+                        print(f"TimeoutException occurred while processing link: {complete_link}")
+                        attempts += 1
+                        time.sleep(1)
+                else:
+                    print(f"Maximum retry attempts reached for link: {complete_link}. Moving to the next link.")
+            # if count == 1:
+            #     break
+
+
 
 
 
@@ -70,3 +126,7 @@ while not exit_loop:
     if user_input.lower() == "off":
         exit_loop = True
 driver.quit()
+
+
+
+
